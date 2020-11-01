c05fb5d9590916169e820f0e1d23bb8446dc5a85
vikbytes <55359319+vikbytes@users.noreply.github.com>
2020-11-01 08:26:38 +0100

    Made icon, fixed about window on build, inplemented storage functionality.

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-const { app, BrowserWindow, Menu, Tray } = require('electron');
+const { app, BrowserWindow, Menu, Tray, nativeImage } = require('electron');
+const path = require('path')
+
+const activeIconPath = path.join(__dirname, 'assets/tomat-active.png')
+const inactiveIconPath = path.join(__dirname, 'assets/tomat-inactive.png')
+const breakIconPath = path.join(__dirname, 'assets/tomat-break.png')
+
+const Store = require('electron-store');
+const store = new Store();
 
 try {
   require('electron-reloader')(module)
 } catch (_) {}
 
+app.dock.hide()
 let tray = null
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    show: false,
-    backgroundColor: '#222222'
+    width: 640,
+    height: 480,
+    show: false
   })
 
   mainWindow.loadFile('index.html')
@@ -20,31 +28,43 @@ function createWindow() {
 
 const handleClick = (menuItem, BrowserWindow, event) => {
   if (menuItem.label === 'Enabled') {
-    console.log("Enabled")
-    tray.setImage('./assets/tomat-active.png')
+    tray.setImage(nativeImage.createFromPath(activeIconPath))
+    store.set('state', 'enabled');
   } else if (menuItem.label === 'Disabled') {
-    console.log("Disabled")
-    tray.setImage('./assets/tomat-inactive.png')
-  } else if (menuItem.label === 'Quit') {
-    app.quit()
-  } 
+    tray.setImage(nativeImage.createFromPath(inactiveIconPath))
+    store.set('state', 'disabled')
+  } else if (menuItem.label === 'Settings') {
+    createWindow()
+  }
   else {
     console.log(menuItem.label)
   }
-  //console.log(menuItem + " clicked in " + BrowserWindow + " by " + event);
 }
 
-app.whenReady().then(() => {
-  tray = new Tray('./assets/tomat-inactive.png')
+app.on('ready', () => {
+  let state = store.get('state')
+  let image;
+  if (state === 'enabled') {
+    image = nativeImage.createFromPath(activeIconPath)
+  } else if (state === 'disabled') {
+    image = nativeImage.createFromPath(inactiveIconPath) 
+  } else {
+    image = nativeImage.createFromPath(inactiveIconPath)
+  }
+  tray = new Tray(image)
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Enabled', type: 'radio', click: handleClick},
+    { label: 'Tomat', type: 'normal', enabled: false},
+    { label: 'About', type: 'normal', role: 'about'},
+    { label: 'Settings', type: 'normal', click: handleClick },
+    { label: "sep", type: 'separator'},
+    { label: 'Enabled', type: 'radio', click: handleClick, checked: false},
     { label: 'Disabled', type: 'radio', click: handleClick, checked: true},
     { label: "sep", type: 'separator'},
-    { label: "Quit", type: 'normal', click: handleClick}
+    { label: "Quit", type: 'normal', role: 'quit'}
   ])
-  tray.setToolTip('Tomat')
+  tray.setToolTip('tomat')
   tray.setContextMenu(contextMenu)
-  createWindow()
+  //createWindow()
 })
 
 app.on('window-all-closed', () => {
@@ -55,7 +75,7 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    //createWindow()
   }
 })
 
