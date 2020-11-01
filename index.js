c9fc5432ebb619f612c1676736e446d9c45e12b7
vikbytes <55359319+vikbytes@users.noreply.github.com>
2020-11-01 01:13:52 +0100

    Made icons for the three states. Fixed the status bar menu.

--- index.js
+++ index.js
@@ -18,11 +18,29 @@ function createWindow() {
   mainWindow.once('ready-to-show', mainWindow.show)
 }
 
+const handleClick = (menuItem, BrowserWindow, event) => {
+  if (menuItem.label === 'Enabled') {
+    console.log("Enabled")
+    tray.setImage('./assets/tomat-active.png')
+  } else if (menuItem.label === 'Disabled') {
+    console.log("Disabled")
+    tray.setImage('./assets/tomat-inactive.png')
+  } else if (menuItem.label === 'Quit') {
+    app.quit()
+  } 
+  else {
+    console.log(menuItem.label)
+  }
+  //console.log(menuItem + " clicked in " + BrowserWindow + " by " + event);
+}
+
 app.whenReady().then(() => {
-  tray = new Tray('./tomat-inactive.png')
+  tray = new Tray('./assets/tomat-inactive.png')
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio'},
-    { label: 'Item2', type: 'radio', checked: true}
+    { label: 'Enabled', type: 'radio', click: handleClick},
+    { label: 'Disabled', type: 'radio', click: handleClick, checked: true},
+    { label: "sep", type: 'separator'},
+    { label: "Quit", type: 'normal', click: handleClick}
   ])
   tray.setToolTip('Tomat')
   tray.setContextMenu(contextMenu)
